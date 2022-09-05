00f9cab6a38a26ab5c1d8f01732704e5d0359d80
jimmyberg <jimmyberg.kim@gmail.com>
2022-09-05 12:50:28 +0900

    import 정리

--- src/main/kotlin/me/jimmyberg/springbatchwithquerydsl/config/job/BatchJobConfig.kt
+++ src/main/kotlin/me/jimmyberg/springbatchwithquerydsl/config/job/BatchJobConfig.kt
@@ -3,9 +3,6 @@ package me.jimmyberg.springbatchwithquerydsl.config.job
 import org.springframework.batch.core.Step
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory
-import org.springframework.batch.core.configuration.annotation.StepBuilderFactory
-import org.springframework.batch.core.step.tasklet.Tasklet
-import org.springframework.batch.repeat.RepeatStatus
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 
